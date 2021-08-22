19f326995a40961e5524dec6beca30fffaf8457e
danik <d.shumakovich@gmail.com>
2021-08-22 19:38:53 +0200

    Add images to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ Encore
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[ext]',
+    })
 
     /*
      * ENTRY CONFIG
