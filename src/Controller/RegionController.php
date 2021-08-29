14f1cdbed61fb36b104cc6129592d8259a1ee52c
danik <d.shumakovich@gmail.com>
2021-08-29 18:33:01 +0200

    Create region controller with index method and template.

---
+++ src/Controller/RegionController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\RegionRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegionController extends AbstractController
+{
+    /**
+     * @Route("/regions", name="regions")
+     *
+     * @param RegionRepository $regionRepository
+     *
+     * @return Response
+     */
+    public function index(RegionRepository $regionRepository): Response
+    {
+        $regions = $regionRepository->findAll();
+
+        return $this->render('region/index.html.twig', [
+            'regions' => $regions,
+        ]);
+    }
+}
