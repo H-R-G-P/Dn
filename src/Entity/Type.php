4ec849b1dc9c5195a17ab437bd64161f405f95c5
danik <d.shumakovich@gmail.com>
2021-09-04 11:26:35 +0200

    Add new columns in Type table.

--- src/Entity/Type.php
+++ src/Entity/Type.php
@@ -29,6 +29,26 @@ class Type
      */
     private $dances;
 
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $dancers;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_group;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_man;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_woman;
+
     public function __construct()
     {
         $this->dances = new ArrayCollection();
@@ -77,6 +97,54 @@ class Type
                 $dance->setType(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getDancers(): ?int
+    {
+        return $this->dancers;
+    }
+
+    public function setDancers(int $dancers): self
+    {
+        $this->dancers = $dancers;
+
+        return $this;
+    }
+
+    public function getIsGroup(): ?bool
+    {
+        return $this->is_group;
+    }
+
+    public function setIsGroup(bool $is_group): self
+    {
+        $this->is_group = $is_group;
+
+        return $this;
+    }
+
+    public function getIsMan(): ?bool
+    {
+        return $this->is_man;
+    }
+
+    public function setIsMan(bool $is_man): self
+    {
+        $this->is_man = $is_man;
+
+        return $this;
+    }
+
+    public function getIsWoman(): ?bool
+    {
+        return $this->is_woman;
+    }
+
+    public function setIsWoman(bool $is_woman): self
+    {
+        $this->is_woman = $is_woman;
 
         return $this;
     }
