fe649cd1a1bb027a809a2aa50904f924475b0cf9
danik <d.shumakovich@gmail.com>
2021-09-19 15:10:14 +0200

    Add tables "place" and "district".

---
+++ migrations/Version20210919130353.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210919130353 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE district (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, department_id INT NOT NULL, UNIQUE INDEX UNIQ_31C154875E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE place (id INT AUTO_INCREMENT NOT NULL, district_id_id INT DEFAULT NULL, category VARCHAR(1) NOT NULL, name VARCHAR(150) NOT NULL, department_id INT NOT NULL, lon DOUBLE PRECISION DEFAULT NULL, lat DOUBLE PRECISION DEFAULT NULL, comment VARCHAR(150) DEFAULT NULL, UNIQUE INDEX UNIQ_741D53CD5E237E06 (name), INDEX IDX_741D53CDD0023964 (district_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE place ADD CONSTRAINT FK_741D53CDD0023964 FOREIGN KEY (district_id_id) REFERENCES district (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE place DROP FOREIGN KEY FK_741D53CDD0023964');
+        $this->addSql('DROP TABLE district');
+        $this->addSql('DROP TABLE place');
+    }
+}
---
+++ src/Entity/Place.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlaceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PlaceRepository::class)
+ */
+class Place
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=1)
+     */
+    private string $category;
+
+    /**
+     * @ORM\Column(type="string", length=150, unique=true)
+     */
+    private string $name;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=District::class, inversedBy="places")
+     */
+    private ?District $district_id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private int $department_id;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private ?float $lon;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private ?float $lat;
+
+    /**
+     * @ORM\Column(type="string", length=150, nullable=true)
+     */
+    private ?string $comment;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getCategory(): string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDistrictId(): ?District
+    {
+        return $this->district_id;
+    }
+
+    public function setDistrictId(?District $district_id): self
+    {
+        $this->district_id = $district_id;
+
+        return $this;
+    }
+
+    public function getDepartmentId(): int
+    {
+        return $this->department_id;
+    }
+
+    public function setDepartmentId(int $department_id): self
+    {
+        $this->department_id = $department_id;
+
+        return $this;
+    }
+
+    public function getLon(): ?float
+    {
+        return $this->lon;
+    }
+
+    public function setLon(float $lon): self
+    {
+        $this->lon = $lon;
+
+        return $this;
+    }
+
+    public function getLat(): ?float
+    {
+        return $this->lat;
+    }
+
+    public function setLat(float $lat): self
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+}
