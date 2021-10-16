cfeb7cca734d4f137e4936f5afa077a60ef2a809
danik <d.shumakovich@gmail.com>
2021-10-16 15:31:34 +0200

    Update RegionController.

--- src/Controller/RegionController.php
+++ src/Controller/RegionController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Region;
+use App\Service\CoordinatesService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\RegionRepository;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,17 +32,30 @@ class RegionController extends AbstractController
      *
      * @param string $slug
      * @param RegionRepository<Region> $regionRepository
+     * @param CoordinatesService $coordinatesService
      *
      * @return Response
      */
-    public function show(string $slug, RegionRepository $regionRepository) : Response
+    public function show(string $slug, RegionRepository $regionRepository, CoordinatesService $coordinatesService) : Response
     {
         $region = $regionRepository->findOneBy([
             'slug' => $slug,
         ]);
 
+        if ($region === null) {
+            return $this->redirectToRoute('homepage');
+        }
+
+        try {
+            $polygon = $coordinatesService->getPolygon($region->getPlaces());
+        }catch (\Exception $e) {
+            $polygon = null;
+        }
+
         return $this->render('region/show.html.twig', [
             'region' => $region,
+            'region_json' => json_encode($region),
+            'polygon' => json_encode($polygon),
         ]);
     }
 }
