9c67f34f3c3f2979fa6dd219fde5153552080339
danik <d.shumakovich@gmail.com>
2021-10-19 18:04:26 +0200

    Fix encoding objects to json in region controller.

--- src/Controller/RegionController.php
+++ src/Controller/RegionController.php
@@ -8,6 +8,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\RegionRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class RegionController extends AbstractController
 {
@@ -52,10 +56,23 @@ class RegionController extends AbstractController
             $polygon = null;
         }
 
+        $encoder = new JsonEncoder();
+        $defaultContext = [
+            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object, $format, $context) {
+                return $object->getName();
+            },
+        ];
+        $normalizer = new ObjectNormalizer(null, null, null, null, null, null, $defaultContext);
+
+        $serializer = new Serializer([$normalizer], [$encoder]);
+
+        $regionJson = $serializer->serialize($region, 'json');
+        $polygonJson = $serializer->serialize($polygon, 'json');
+
         return $this->render('region/show.html.twig', [
             'region' => $region,
-            'region_json' => json_encode($region),
-            'polygon' => json_encode($polygon),
+            'region_json' => $regionJson,
+            'polygon' => $polygonJson,
         ]);
     }
 }
