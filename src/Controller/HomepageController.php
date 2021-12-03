c77d5120a3c9323312d1c97a9c352584bda71d0d
danik <d.shumakovich@gmail.com>
2021-12-03 14:48:51 +0100

    Add displaying departments on homepage.

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -7,6 +7,7 @@ use App\Entity\Region;
 use App\Entity\Source;
 use App\Entity\Type;
 use App\Repository\DanceRepository;
+use App\Repository\DepartmentRepository;
 use App\Repository\RegionRepository;
 use App\Repository\SourceRepository;
 use App\Repository\TypeRepository;
@@ -23,21 +24,29 @@ class HomepageController extends AbstractController
      * @param TypeRepository<Type> $typeRepository
      * @param SourceRepository<Source> $sourceRepository
      * @param DanceRepository<Dance> $danceRepository
+     * @param DepartmentRepository $departmentRepository
      *
      * @return Response
      */
-    public function index(RegionRepository $regionRepository, TypeRepository $typeRepository, SourceRepository $sourceRepository, DanceRepository $danceRepository): Response
+    public function index(RegionRepository $regionRepository, TypeRepository $typeRepository, SourceRepository $sourceRepository, DanceRepository $danceRepository, DepartmentRepository $departmentRepository): Response
     {
         $regions = $regionRepository->findAll();
         $types = $typeRepository->findAll();
         $sources = $sourceRepository->findAll();
         $topTenDances = $danceRepository->findBy([], ['views' => 'DESC'],10);
+        $departments = $departmentRepository->findAll();
+
+        $c = [];
+        foreach ($departments as $department) {
+            $c[] = count($department->getDances());
+        }
 
         return $this->render('homepage/index.html.twig', [
             'regions' => $regions,
             'types' => $types,
             'sources' => $sources,
             'top_ten_dances' => $topTenDances,
+            'departments' => $departments,
         ]);
     }
 }
