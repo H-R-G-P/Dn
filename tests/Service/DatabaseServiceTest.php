ae1c6f006040405e19892d1246de2326e659953e
danik <d.shumakovich@gmail.com>
2022-01-10 16:59:54 +0100

    Add test for method 'getRegionsSortedByDances'.

---
+++ tests/Service/DatabaseServiceTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Entity\Region;
+use App\Service\DatabaseService;
+use App\Vo\DatabaseVO;
+use PHPUnit\Framework\TestCase;
+use function PHPUnit\Framework\assertSame;
+
+class DatabaseServiceTest extends TestCase
+{
+    public function testGetRegionsSortedByDances()
+    {
+        $regions = [];
+        $region = $this->createMock(Region::class);
+        $region->expects($this->any())
+            ->method('getDances')
+            ->willReturn([]);
+        $regions[] = $region;
+        $region = $this->createMock(Region::class);
+        $region->expects($this->any())
+            ->method('getDances')
+            ->willReturn([1, 2, 3]);
+        $regions[] = $region;
+        $region = $this->createMock(Region::class);
+        $region->expects($this->any())
+            ->method('getDances')
+            ->willReturn([2, 3]);
+        $regions[] = $region;
+
+        $database = $this->createMock(DatabaseVO::class);
+        $database->expects($this->any())
+            ->method('getRegions')
+            ->willReturn($regions);
+
+        $service = new DatabaseService($database);
+
+        $sortedRegions = $service->getRegionsSortedByDances();
+        assertSame(3, count($sortedRegions[0]->getDances()));
+        assertSame(2, count($sortedRegions[1]->getDances()));
+        assertSame(0, count($sortedRegions[2]->getDances()));
+    }
+}
