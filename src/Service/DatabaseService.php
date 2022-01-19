ce0747241331b3fdfe81d3c90a4a22ba2732c214
danik <d.shumakovich@gmail.com>
2022-01-19 20:33:41 +0100

    Replace self code to global php function.

--- src/Service/DatabaseService.php
+++ src/Service/DatabaseService.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 
 namespace App\Service;
@@ -179,12 +179,7 @@ class DatabaseService
     {
         $regions = $this->getRegionsSortedByDances();
 
-        $top = [];
-        for ($i = 0; $i < 10; $i++){
-            $top[] = $regions[$i];
-        }
-
-        return $top;
+        return array_slice($regions, 0, 10);
     }
 
     /**
