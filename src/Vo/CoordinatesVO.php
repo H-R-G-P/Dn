a1e5c7615878a6f756a8f2886da3b8efc5b18a4b
danik <d.shumakovich@gmail.com>
2022-01-25 13:55:14 +0100

    Create CoordinatesVO

---
+++ src/Vo/CoordinatesVO.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+
+namespace App\Vo;
+
+
+class CoordinatesVO
+{
+    /** Latitude
+     * @var float
+     */
+    private float $lat;
+
+    /** Longitude
+     * @var float
+     */
+    private float $lon;
+
+    public function __construct(float $lat, float $lon)
+    {
+        $this->lat = $lat;
+        $this->lon = $lon;
+    }
+
+    /** Get Latitude
+     * @return float
+     */
+    public function getLat(): float
+    {
+        return $this->lat;
+    }
+
+    /** Get Longitude
+     * @return float
+     */
+    public function getLon(): float
+    {
+        return $this->lon;
+    }
+}
