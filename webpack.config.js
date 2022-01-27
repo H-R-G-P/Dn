da050f4cff081dffd9de6dc261a7292c200e5f3a
danik <d.shumakovich@gmail.com>
2022-01-27 13:03:06 +0100

    Fix name of js file in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addEntry('regions', './assets/regions.js')
+    .addEntry('regions', './assets/map.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
