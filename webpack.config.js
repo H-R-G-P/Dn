90ef7787f0124ee70b384849bf3d3f274216cc30
danik <d.shumakovich@gmail.com>
2022-02-04 18:54:24 +0100

    Rename map js file

--- webpack.config.js
+++ webpack.config.js
@@ -25,8 +25,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    // TODO Rename entry point to 'map'
-    .addEntry('regions', './assets/map.js')
+    .addEntry('map', './assets/map.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
