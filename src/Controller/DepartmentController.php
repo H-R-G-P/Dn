134f9dcb0a1fc4249c416635a9b07a1502d4e7a3
danik <d.shumakovich@gmail.com>
2022-02-06 18:35:43 +0100

    Add displaying list of regions in department page page

--- src/Controller/DepartmentController.php
+++ src/Controller/DepartmentController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Department;
 use App\Entity\Place;
+use App\Entity\Region;
 use App\Repository\DepartmentRepository;
 use App\Repository\PlaceRepository;
+use App\Repository\RegionRepository;
+use App\Service\DatabaseService;
 use App\Service\MapService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,10 +29,12 @@ class DepartmentController extends AbstractController
      * @param DepartmentRepository<Department> $departmentRepository
      * @param MapService $mapService
      * @param PlaceRepository<Place> $placeRepository
+     * @param DatabaseService $databaseService
+     * @param RegionRepository<Region> $regionRepository
      *
      * @return Response
      */
-    public function show(string $slug, DepartmentRepository $departmentRepository, MapService $mapService, PlaceRepository $placeRepository): Response
+    public function show(string $slug, DepartmentRepository $departmentRepository, MapService $mapService, PlaceRepository $placeRepository, DatabaseService $databaseService, RegionRepository $regionRepository): Response
     {
         $department = $departmentRepository->findOneBy([
             'slug' => $slug,
@@ -41,12 +46,15 @@ class DepartmentController extends AbstractController
         }
 
         $places = $placeRepository->findByEntityExtended($department);
+        $regions = $regionRepository->findByDepartment($department);
+        $regions = $databaseService->setDances($regions);
 
         $map = $mapService->createMapDTO($places);
         $map_json = $map === null ? null : $map->serializeToJson();
 
         return $this->render('department/show.html.twig', [
             'department' => $department,
+            'regions' => $regions,
             'map_json' => $map_json,
         ]);
     }
