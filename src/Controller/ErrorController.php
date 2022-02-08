846f08cc7510613528d99c09a5de907b965803be
danik <d.shumakovich@gmail.com>
2022-02-08 18:10:49 +0100

    Update ErrorController

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -6,23 +6,28 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class ErrorController extends AbstractController
 {
     /**
      * @param Throwable $exception
+     * @param Request $request
      *
      * @return Response
+     *
+     * @throws Throwable
      */
-    public function show(Throwable $exception): Response
+    public function show(Throwable $exception, Request $request): Response
     {
-        if ($exception->getCode() === 404){
+        if ($request->attributes->get('exception') instanceof NotFoundHttpException){
             $this->addFlash('warning', $exception->getMessage());
             return $this->redirectToRoute('homepage');
         }
 
-        throw new \Exception($exception->getMessage(), $exception->getCode(), $exception);
+        throw $exception;
     }
 }
