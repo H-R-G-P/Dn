7cffc8252034fda0f8b6901e556eb3dae58a99fe
danik <d.shumakovich@gmail.com>
2022-02-08 19:56:15 +0100

    Add method for finding all sources with version property

--- src/Repository/SourceRepository.php
+++ src/Repository/SourceRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Source;
+use App\Entity\Version;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -20,6 +21,28 @@ class SourceRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Source::class);
+    }
+
+    /**
+     * @return Source[]
+     */
+    public function findAllWithVersions(): array
+    {
+        $em = $this->getEntityManager();
+        $versionRepository = $em->getRepository(Version::class);
+
+        $query = $em->createQuery(
+            'SELECT s
+            FROM App\Entity\Source s'
+        );
+
+        $sources = $query->getResult();
+
+        foreach ($sources as $source) {
+            $source->setVersions($versionRepository->findBySource($source));
+        }
+
+        return $sources;
     }
 
     // /**
