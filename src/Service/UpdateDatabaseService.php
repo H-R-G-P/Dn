a9da183868c4cd3f61160666781fb2bda2f2db86
danik <d.shumakovich@gmail.com>
2022-02-08 18:28:29 +0100

    Add method for increasing views for version

--- src/Service/UpdateDatabaseService.php
+++ src/Service/UpdateDatabaseService.php
@@ -5,6 +5,7 @@ namespace App\Service;
 
 
 use App\Entity\Dance;
+use App\Entity\Version;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
@@ -28,12 +29,25 @@ class UpdateDatabaseService
     /** Increase view count by 1
      * @param Dance $dance
      */
-    public function increaseViews(Dance $dance): void
+    public function increaseDanceViews(Dance $dance): void
     {
         $session = $this->stack->getSession();
         if (!$session->has($dance->getId().'Dance')) {
             $session->set($dance->getId().'Dance', 'This dance already was viewed.');
             $dance->subView();
+            $this->em->flush();
+        }
+    }
+
+    /** Increase view count by 1
+     * @param Version $version
+     */
+    public function increaseVersionViews(Version $version): void
+    {
+        $session = $this->stack->getSession();
+        if (!$session->has($version->getId().'Version')) {
+            $session->set($version->getId().'Version', 'This dance already was viewed.');
+            $version->subView();
             $this->em->flush();
         }
     }
