0a396b57fad000a5a88a6d91e5d8ccb25359365c
danik <d.shumakovich@gmail.com>
2022-03-30 11:49:50 +0200

    Drop unused video column 'link'.

--- src/Entity/Video.php
+++ src/Entity/Video.php
@@ -25,11 +25,6 @@ class Video
     private int $type;
 
     /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private string $link;
-
-    /**
      * @ORM\Column(type="string", length=30, nullable=true)
      */
     private string $youtubeId;
@@ -103,21 +98,5 @@ class Video
     public function setYoutubeId(string $youtubeId): void
     {
         $this->youtubeId = $youtubeId;
-    }
-
-    /**
-     * @return string
-     */
-    public function getLink(): string
-    {
-        return $this->link;
-    }
-
-    /**
-     * @param string $link
-     */
-    public function setLink(string $link): void
-    {
-        $this->link = $link;
     }
 }
